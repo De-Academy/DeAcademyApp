3eb7ecb3b282ed44a4b8136af33d4dc256a45ad5
Josef Bruseke <josefbruseke@gmail.com>
2024-03-22 00:33:59 -0300

    initial commit

---
+++ deacademy/src/app/page.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Image from "next/image";
+import logo from "./assets/logo.png"
+import discord_logo from "./assets/discord-logo.png"
+
+
+const page = () => {
+  return (
+    <div className="h-dvh bg-gradient-to-b from-purple-900 from-37% to-purple-100 to-80%">
+      <nav>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex-shrink-0 mt-4">
+              <a href="/" className="text-white font-bold text-xl flex items-center">
+                <Image
+                  src={logo}
+                  alt="Logo"
+                  id="logo"
+                  className="mr-2"
+                  width={50}
+                  height={50}
+                />
+                DeAcademy
+              </a>
+            </div>
+            <div className="hidden md:flex items-center mt-6">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a href="#" className="text-gray-100 hover:bg-purple-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">What We Are</a>
+                <a href="#" className="text-gray-100 hover:bg-purple-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium">RoadMap</a>
+              </div>
+
+              <a href="https://discord.gg/EFJB3s3U" target="_blank" className="text-white font-bold text-xl flex items-center ml-4  hover:bg-purple-900 rounded-md px-1 py-1">
+                <Image
+                  src={discord_logo}
+                  alt="Logo"
+                  className=""
+                  width={30}
+                  height={15}
+                />
+              </a>
+            </div>
+            <div className="md:hidden">
+              <button type="button" className="text-gray-500 hover:text-white focus:outline-none focus:text-white">
+                <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                  <path fillRule="evenodd" d="M3 5h18a1 1 0 0 1 0 2H3a1 1 0 1 1 0-2zm0 6h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 6h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2z" clipRule="evenodd"></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>  
+      <div className="hidden md:flex items-center justify-center mt-44">
+        <div className="flex flex-col items-center">
+          <h1 id="main-text-home-page" className="text-6xl font-bold text-gray-100" >Learn and Earn</h1>
+          <h1 id="main-text-home-page" className="text-6xl font-bold text-gray-100">Teach and Reach</h1>
+          <div className="flex flex-col items-center mt-8">
+            <p id="sub-text-home-page" className="text-6x1 font-bold text-gray-100">Bringing a new form of promoting education.</p>
+            <p id="sub-text-home-page" text-32 className="text-6x1 font-bold text-gray-100">Descentralized, Easy and Fun.</p>
+          </div>
+        </div>
+      </div>
+      
+
+
+
+
+
+    </div>
+  )
+}
+
+export default page
