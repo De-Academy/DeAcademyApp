afeae15ac7057061b4588018f0fd126b4f44bb51
Eduardo-Bocon <eduardo.h.p.bocon@gmail.com>
2024-03-25 16:08:25 -0300

    new sidebar done

--- deacademy/src/app/sideBar.tsx
+++ deacademy/src/app/sideBar.tsx
@@ -1,71 +1,70 @@
 
-import logo from "./assets/logo2.png"
-import courses from "./assets/courses.png"
-import myCourses from "./assets/my courses.png"
-import marketplace from "./assets/marketplace.png"
-import overview from "./assets/painel.png"
+import logo from "./assets/logo e nome 3.png"
+import courses from "./assets/courses-black.png"
+import myCourses from "./assets/my courses-black.png"
+import marketplace from "./assets/marketplace-black.png"
+import overview from "./assets/painel-black.png"
 import Image from "next/image";
 
 export default function sideBar() {
     return (
-        <aside className="h-screen w-52 fixed top-0 left-0 justify-center">
-            <div className="bg-gradient-to-b from-violet-700 to-cyan-400 h-full flex flex-col items-center">
+        <aside className="h-screen w-72 fixed top-0 left-0 justify-center">
+            <div className="bg-white border-r-2 border-gray-300 h-full flex flex-col items-center">
 
                 {/*logo e nome*/}
-                <a href="/"  className="rounded-full h-20 w-20  mt-5">
-                            <Image src={logo} alt="DeAcademyIcon" className="rounded-full h-20 w-20"/>
-                </a>
-
-                <a href="/" className="mb-16 h-0 p-0">
-                    <h3 className="font-bold text-white text-2xl">DeAcademy</h3>
-                </a>
+                <div className="flex flex-row justify-center items-center m-4 mb-14">
+                    <a href="/"  className="mt-5">
+                                <Image src={logo} alt="DeAcademyIcon" className=""/>
+                    </a>
+                </div>
+                
 
                 {/*icones*/}
                 <ul>
                     <li>
-                        <div className="flex flex-row items-center my-4">
+                        <div className="flex flex-row items-center my-8">
                            <a href="/StudentPages/Courses">
-                                <Image src={courses} alt="Courses Icon" className=" h-10 w-10"/>
+                                <Image src={courses} alt="Courses Icon" className=" h-14 w-14"/>
                             </a>
                             <a href="/StudentPages/Courses">
-                                <h2 className="font-bold text-white mx-2">Courses</h2>
+                                <h2 className="font-semibold text-2xl text-black mx-2">Courses</h2>
                                 </a> 
                         </div>
                     </li>
                     <li>
-                        <div className="flex flex-row items-center my-4">
+                        <div className="flex flex-row items-center my-8">
                            <a href="/StudentPages/MyCourses">
-                                <Image src={myCourses} alt="myCourses Icon" className=" h-10 w-10"/>
+                                <Image src={myCourses} alt="myCourses Icon" className=" h-14 w-14"/>
                             </a>
                             <a href="/StudentPages/MyCourses">
-                                <h2 className="font-bold text-white mx-2">My courses</h2>
+                                <h2 className="font-semibold text-2xl text-black mx-2">My courses</h2>
                             </a> 
                         </div>
                     </li>
                     <li>
-                        <div className="flex flex-row items-center my-4">
+                        <div className="flex flex-row items-center my-8">
                            <a href="/StudentPages/Marketplace">
-                                <Image src={marketplace} alt="myCourses Icon" className=" h-10 w-10"/>
+                                <Image src={marketplace} alt="myCourses Icon" className=" h-14 w-14"/>
                             </a>
                             <a href="/StudentPages/Marketplace">
-                                <h2 className="font-bold text-white mx-2">Marketplace</h2>
+                                <h2 className="font-semibold text-2xl text-black mx-2">Marketplace</h2>
                             </a> 
                         </div>
                     </li>
                     <li>
-                        <div className="flex flex-row items-center my-4">
+                        <div className="flex flex-row items-center my-8">
                            <a href="/StudentPages/Overview">
-                                <Image src={overview} alt="myCourses Icon" className=" h-10 w-10"/>
+                                <Image src={overview} alt="myCourses Icon" className=" h-14 w-14"/>
                             </a>
                             <a href="/StudentPages/Overview">
-                                <h2 className="font-bold text-white px-2">Overview</h2>
+                                <h2 className="font-semibold text-2xl text-black px-2">Overview</h2>
                             </a> 
                         </div>
                     </li>
                 </ul>
 
                 {/*Student/teacher bottom button*/}
-                <button className="mt-96 rounded-full bg-gradient-to-r from-violet-700 to-cyan-600 px-10 py-2">
+                <button className="mt-80 rounded-full bg-gradient-to-r from-violet-700 to-cyan-600 px-10 py-2">
                     <p className="font-bold text-white">Student</p>
                 </button>
             </div>
