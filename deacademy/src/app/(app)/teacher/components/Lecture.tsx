393316a6cf88b6c59d07cc25cd70724fe7703619
Josef Bruseke <josefbruseke@gmail.com>
2024-03-31 17:29:54 -0300

    add lecture count

--- deacademy/src/app/(app)/teacher/components/Lecture.tsx
+++ deacademy/src/app/(app)/teacher/components/Lecture.tsx
@@ -4,15 +4,21 @@ import UploadButton from "./UploadButton";
 import { TextInput } from "./textInput";
 import TextArea from "./TextArea";
 
-const Lecture = () => {
+
+interface LectureProps {
+    number: number
+}
+
+
+const Lecture = ({number}: LectureProps) => {
     const [active, setActive] = useState<'content' | 'description'>('content')
 
 
     return (
-        <main className="">
+        <main className="border-black border-2 rounded-lg m-2 p-4">
             <div className="flex">
                     <div className="flex justify-center items-center">
-                        <p>Lecture 1</p>
+                        <p>Lecture {number}</p>
                         <TextInput label="" placeholder=""></TextInput>
                     </div>
                     <div className="flex gap-10 ml-20">
