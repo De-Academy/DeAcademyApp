b8c28523f98d02bc44688618a3dde38f7ee8ebf2
Josef Bruseke <josefbruseke@gmail.com>
2024-04-10 15:30:18 -0300

    fix replacement

--- deacademy/src/app/(app)/teacher/components/Lecture.tsx
+++ deacademy/src/app/(app)/teacher/components/Lecture.tsx
@@ -19,20 +19,16 @@ const Lecture = ({number, onRemove}: LectureProps) => {
 
     return (
         <main className="border-gray-300 border-2 rounded-lg m-2 p-4">
-            <div className="lg:flex justify-center ">
-                    <div className="flex justify-center items-center  text-purple-800 font-bold">
-                        <p>Lecture {number}</p>
-                        <div className="w-4/6">
-                        <TextInput label="" placeholder="Title"></TextInput>
-                        </div>
-                    </div>  
-                    <div className="flex gap-6 text-[#1B1B1B] items-center w-1/2">
-                        <Button color={active === 'content' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'} onClick={() => setActive('content')} className="hover:bg-[#1B1B1B] hover:text-white h-1/2 w-1/2">Content</Button>
-                        <Button color={active === 'description' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'}  onClick={() => setActive('description')} className="hover:bg-[#1B1B1B] hover:text-white h-1/2 w-1/2">Description</Button>    
-                    </div>
-                    <div>
-                        <button onClick={onRemove} className="hover:bg-[#1B1B1B] rounded-full p-3 ml-2 mt-2"> X </button>
-                    </div>
+            <div className="lg:flex justify-between ">
+                <div className="flex justify-start items-center text-purple-800 font-bold">
+                    <p>Lecture {number}</p>
+                    <TextInput label="" placeholder="Title"></TextInput>
+                </div>  
+                <div className="flex gap-6 text-[#1B1B1B] justify-end items-center ">
+                    <Button color={active === 'content' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'} onClick={() => setActive('content')} className="hover:bg-[#1B1B1B] hover:text-white h-1/2 w-1/3">Content</Button>
+                    <Button color={active === 'description' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'}  onClick={() => setActive('description')} className="hover:bg-[#1B1B1B] hover:text-white h-1/2 w-1/3">Description</Button>
+                    <button onClick={onRemove} className="hover:bg-[#1B1B1B] rounded-full p-3 m-2"> X </button>    
+                </div>
             </div>
             {active === 'content' &&(
             <div className="flex justify-center m-16">   
--- deacademy/src/app/(app)/teacher/components/textInput.tsx
+++ deacademy/src/app/(app)/teacher/components/textInput.tsx
@@ -11,7 +11,7 @@ export const TextInput = ({label, placeholder} : TextInputProps) => {
                 <label className="block mb-2 text-sm font-medium text-purple-800">{ label }</label>
                     <input 
                     type="text" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 " 
                     placeholder={placeholder} 
                     required />
             </div>    
