8b1744a24a18936d3e890d0996b5a9db22408ac4
Josef Bruseke <josefbruseke@gmail.com>
2024-04-10 14:13:13 -0300

    fix text alignment

--- deacademy/src/app/(app)/teacher/components/Lecture.tsx
+++ deacademy/src/app/(app)/teacher/components/Lecture.tsx
@@ -17,9 +17,11 @@ const Lecture = ({number}: LectureProps) => {
     return (
         <main className="border-gray-300 border-2 rounded-lg m-2 p-4">
             <div className="lg:flex justify-center">
-                    <div className="flex justify-center items-center text-purple-800 font-bold">
+                    <div className="flex justify-center items-center  text-purple-800 font-bold">
                         <p>Lecture {number}</p>
+                        <div className="w-4/6">
                         <TextInput label="" placeholder="Title"></TextInput>
+                        </div>
                     </div>  
                     <div className="flex gap-10 text-[#1B1B1B]">
                         <Button color={active === 'content' ? 'bg-[#1B1B1B] text-white' : 'bg-[#EFEDED]'} onClick={() => setActive('content')} className="hover:bg-[#1B1B1B] hover:text-white h-full w-full">Content</Button>
