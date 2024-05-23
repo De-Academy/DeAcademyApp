a10c38a8b54d42b5ffca78fe0809b286d1e04334
Josef Bruseke <josefbruseke@gmail.com>
2024-05-23 13:44:22 -0300

    compontize course mock-up

--- deacademy/src/app/(app)/student/courses/page.tsx
+++ deacademy/src/app/(app)/student/courses/page.tsx
@@ -50,22 +50,16 @@ export default function Page() {
                 </div>
             </div>
 
-            /* courses */
-            <div className=" mt-28 h-full w-98 fixed top-4 left-80 flex justify-start bg-[#F4F6F9]">
-                <div className="container mx-auto p-4">
+            <div className="mt-4  h-full w-98 top-4 left-80 flex justify-center ">
+                <div className="container p-4">
                     <div className="flex flex-wrap -mx-2">
-                        {images.map((src, index) => (
-                            <div key={index} className="w-80 p-2 mx-2">
-                                <div className="bg-white p-4 rounded shadow-md h-60 cursor-pointer hover:-translate-y-1 drop-shadow-xl hover:drop-shadow-2xl flex justify-start place-items-start flex-col">
-                                    <img src={src.image} alt={`Imagem ${index + 1}`} className="w-72 h-32 rounded"/>
-                                    <p>{src.title}</p>
-                                    <p className="text-[14px]">{src.desc}</p>
-                                    <div className="flex flex-row place-self-end justify-self-end h-5 w-10 align-middle items-center mx-4">
-                                        <p className="mx-2">{src.price}</p>
-                                        <img src="/images/solana.png" alt="" className="h-4 w-4 "/>
-                                    </div>
-                                </div>
-                            </div>
+                        {courses.map((course) => (
+                            <CourseCard 
+                                id={course.id}
+                                image={course.image}
+                                title={course.title}
+                                desc={course.desc}
+                                price={course.price} />
                         ))}
                     </div>
                 </div>
